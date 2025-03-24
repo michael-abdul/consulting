48eea19d927795caa61f775d10cd6bd6f787329f
Sheeyx <sheeyx00@gmail.com>
2025-03-24 17:36:54 +0900

    fix: credentials

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,22 @@ import cors from "cors";
 // 1-ENTRANCE
 
 const app = express();
+const allowedOrigins = ["https://www.studify.uz"]; // Add more if needed
 // console.log("__dirname",__dirname)
+app.use(
+    cors({
+      origin: allowedOrigins,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("./uploads"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
-app.use(cors({credentials: true, origin: true}))
 // 3-VIEWS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
